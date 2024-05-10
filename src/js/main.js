2fe76452a5c1236f51f917bbcf2e3951cfb1d33a
William Gabriel Braga <70717232+Yckson@users.noreply.github.com>
2024-05-10 20:33:53 -0300

    Ajustanto raíz para o github

--- src/js/main.js
+++ src/js/main.js
@@ -55,7 +55,7 @@ $(document).ready(function() {
     getParamsURL();
 
     $('nav img').click(function(){
-        window.location.href = '/';
+        window.location.href = '/prototipo1-divulgaai/';
     });
 
 
